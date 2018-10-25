6e1b5f82f69b7ef8e16022477cd9e31a93a0d85f
marcelhesse1337 <43790289+marcelhesse1337@users.noreply.github.com>
2018-10-25 08:55:36 +0200

    Update marcel.py

--- marcel.py
+++ marcel.py
@@ -8,9 +8,9 @@ import argparse
 # --------------------------------------------- Start Settings ----------------------------------------------------
 HOST = "irc.twitch.tv"                          # Hostname of the IRC-Server in this case twitch's
 PORT = 6667                                     # Default IRC-Port
-CHAN = "#QueenLisa666"                               # Channelname = #{Nickname}
-NICK = "QueenLisa666"                         # Nickname = Twitch username
-#PASS = "oauth:59ogtl9dtrmbj03ljfmpy0y7veadn1"   # www.twitchapps.com/tmi/ will help to retrieve the required authkey
+CHAN = "#AmyCandyLand"                               # Channelname = #{Nickname}
+NICK = "AmyCandyLand"                         # Nickname = Twitch username
+PASS = "oauth:k99icmlw1i3fz7dvw1fbln2h3gt38i"   # www.twitchapps.com/tmi/ will help to retrieve the required authkey
 # --------------------------------------------- End Settings -------------------------------------------------------
 
 
