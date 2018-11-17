0868ca209d6b99804cedb4bdea67ca0afad04cdb
marcelhesse1337 <43790289+marcelhesse1337@users.noreply.github.com>
2018-11-17 09:50:14 +0100

    Update marcel.py

--- marcel.py
+++ marcel.py
@@ -10,7 +10,7 @@ HOST = "irc.twitch.tv"                          # Hostname of the IRC-Server in 
 PORT = 6667                                     # Default IRC-Port
 CHAN = "#SophieSnowy"                               # Channelname = #{Nickname}
 NICK = "SophieSnowy"                         # Nickname = Twitch username
-#PASS = "oauth:1bsu35muu14gdvbxm517qcpx0mue13"   # www.twitchapps.com/tmi/ will help to retrieve the required authkey
+PASS = "oauth:1bsu35muu14gdvbxm517qcpx0mue13"   # www.twitchapps.com/tmi/ will help to retrieve the required authkey
 # --------------------------------------------- End Settings -------------------------------------------------------
 
 
